9546ad9bd4521cd52a75b6e6cb4631f5ec904eae
keithpitt <me@keithpitt.com>
2011-08-07 17:58:50 +0800

    Added a method to calculate the distance between a number of places

--- lib/trains/conductor.rb
+++ lib/trains/conductor.rb
@@ -1,10 +1,40 @@
 class Conductor
 
+  class NoPathError < StandardError; end
+
   attr_reader :places, :routes
 
   def initialize
     @places = []
     @routes = []
   end
+
+  def distance_between(*places)
+    distance = 0
+
+    last_place = nil
+    places.each do |place|
+      distance += find_route_between(last_place, place).distance if last_place
+
+      last_place = place
+    end
+
+    distance
+  end
+
+  private
+
+    def find_route_between(origin, destination)
+      route_map["#{origin.name}#{destination.name}"] or raise NoPathError
+    end
+
+    # Construct a hash of the origin and destinations by name, example:
+    #   { "AB" => #<Route>, "BA" => #<Route> }
+    def route_map
+      @route_map ||= @routes.inject({}) do |result, element|
+        result["#{element.origin.name}#{element.destination.name}"] = element
+        result
+      end
+    end
 
 end
--- spec/lib/conductor_spec.rb
+++ spec/lib/conductor_spec.rb
@@ -12,28 +12,28 @@ describe Conductor do
   #
   # Graph: AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7
 
-  let (:place_a) { Place.new(:name => "A") }
-  let (:place_b) { Place.new(:name => "B") }
-  let (:place_c) { Place.new(:name => "C") }
-  let (:place_d) { Place.new(:name => "D") }
-  let (:place_e) { Place.new(:name => "E") }
+  let (:a) { Place.new(:name => "A") }
+  let (:b) { Place.new(:name => "B") }
+  let (:c) { Place.new(:name => "C") }
+  let (:d) { Place.new(:name => "D") }
+  let (:e) { Place.new(:name => "E") }
 
   before :all do
-    subject.places << place_a
-    subject.places << place_b
-    subject.places << place_c
-    subject.places << place_d
-    subject.places << place_e
+    subject.places << a
+    subject.places << b
+    subject.places << c
+    subject.places << d
+    subject.places << e
 
-    subject.routes << Route.new(:origin => place_a, :destination => place_b, :distance => 5)
-    subject.routes << Route.new(:origin => place_b, :destination => place_c, :distance => 4)
-    subject.routes << Route.new(:origin => place_c, :destination => place_d, :distance => 8)
-    subject.routes << Route.new(:origin => place_d, :destination => place_c, :distance => 8)
-    subject.routes << Route.new(:origin => place_d, :destination => place_e, :distance => 6)
-    subject.routes << Route.new(:origin => place_a, :destination => place_d, :distance => 5)
-    subject.routes << Route.new(:origin => place_c, :destination => place_e, :distance => 2)
-    subject.routes << Route.new(:origin => place_e, :destination => place_b, :distance => 3)
-    subject.routes << Route.new(:origin => place_a, :destination => place_e, :distance => 7)
+    subject.routes << Route.new(:origin => a, :destination => b, :distance => 5)
+    subject.routes << Route.new(:origin => b, :destination => c, :distance => 4)
+    subject.routes << Route.new(:origin => c, :destination => d, :distance => 8)
+    subject.routes << Route.new(:origin => d, :destination => c, :distance => 8)
+    subject.routes << Route.new(:origin => d, :destination => e, :distance => 6)
+    subject.routes << Route.new(:origin => a, :destination => d, :distance => 5)
+    subject.routes << Route.new(:origin => c, :destination => e, :distance => 2)
+    subject.routes << Route.new(:origin => e, :destination => b, :distance => 3)
+    subject.routes << Route.new(:origin => a, :destination => e, :distance => 7)
   end
 
   context 'properties' do
@@ -44,6 +44,23 @@ describe Conductor do
 
     it 'should have a routes array' do
       subject.routes.should be_kind_of(Array)
+    end
+
+  end
+
+  describe '#distance_between' do
+
+    it 'should return the correct distance between routes' do
+      subject.distance_between(a, b, c).should == 9
+      subject.distance_between(a, d).should == 5
+      subject.distance_between(a, d, c).should == 13
+      subject.distance_between(a, e, b, c, d).should == 22
+    end
+
+    it 'should raise an error if no path can be found' do
+      expect do
+        subject.distance_between(a, e, d)
+      end.should raise_error(Conductor::NoPathError)
     end
 
   end
